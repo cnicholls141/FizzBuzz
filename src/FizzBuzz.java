35a76237b13a001a99d59c95be7dab9402c6f0f8
Chris Nicholls <chris_nicholls@hotmail.co.uk>
2015-12-03 20:00:09 +0000

    initialised

---
+++ src/FizzBuzz.java
@@ -0,0 +1,43 @@
+
+public class FizzBuzz {
+
+	private int dividend;
+	private int divisor;
+	
+	
+	
+	public FizzBuzz() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String calculate (int number) {
+		
+		//number = dividend;
+		
+		if (isDivisibleBy(number, 5) && !isDivisibleBy(number, 3)) {
+			return "Buzz";
+		}
+		if (isDivisibleBy(number, 3) && !isDivisibleBy(number, 3)) {
+			return "Fuzz";
+		}
+		if (isDivisibleBy(number, 3) && isDivisibleBy(number, 5)) {
+			return "FizzBuzz";
+		}
+		else {
+			return Integer.toString(number);
+		}
+	}
+	
+	private boolean isDivisibleBy(int dividend, int divisor) {
+		
+		if (dividend % divisor == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+
+
+}
